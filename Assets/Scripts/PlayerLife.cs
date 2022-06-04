48b957a9948ac8776d5fea6267649a366ddd6077
Subarna Pokharel <subarnapokharel252@gmail.com>
2022-06-04 06:05:54 +0545

    Player-Death-Mechanism

---
+++ Assets/Scripts/PlayerLife.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerLife : MonoBehaviour
+{
+    Animator anim;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim=GetComponent<Animator>();
+        rb=GetComponent<Rigidbody2D>();
+    }
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.CompareTag("Trap"))
+        {
+            Die();
+        }
+    }
+
+    void Die(){
+        rb.bodyType=RigidbodyType2D.Static;
+        anim.SetTrigger("death");
+
+    }
+
+    void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+   
+}
